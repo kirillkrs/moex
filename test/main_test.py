f7d6318e7189ae9d1a51a19ae871c6f4750be341
KK <kirill.krasnov64@gmail.com>
2023-11-02 18:39:58 +0300

    Переработал функцию, временно убрал скользящие средние

--- test/main_test.py
+++ test/main_test.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.dates as mdates
 
-# logging.basicConfig(filename='../logs/bot_func.log', level=logging.INFO,
+# logging.basicConfig(filename='logs/bot_func.log', level=logging.INFO,
 #                     format='%(asctime)s - %(levelname)s - %(message)s')
 
 
@@ -19,10 +19,7 @@ def get_tick(
         interval_name: str = 'недели',
         interval: int = 4,
         time_interval: int = 24,
-        rolling_avg_num: Optional[int] = 3,
-        rolling_avg_int: tuple = (7, 14, 21),
         colors: list = ['#33CC00', '#CC33CC', '#FF6600'],
-
 ):
     try:
         logging.info(
@@ -33,7 +30,6 @@ def get_tick(
         :param interval_name: Желаемый интервал (Дни / недели)
         :param interval: Количество дней / недель
         :param time_interval: временной интервал внутри сессии (60 минут / 24 часа)
-        :param rolling_avg_int: Рассчет
         :param colors: Цета отображения графиков скользящего среднего
         :return: Возвращает графики
         """
@@ -42,12 +38,6 @@ def get_tick(
             date_start = timedelta(weeks=interval)  # From what
         elif interval_name == 'дни' or interval_name == 'дней':
             date_start = timedelta(days=interval)
-
-            # Удалить
-        # elif interval_name == 'часа' or interval_name == 'часов':
-        #     date_start = timedelta(hours=interval)
-        # elif interval_name == 'минуты' or interval_name == 'минут':
-        #     date_start = timedelta(minutes=interval)
 
         date_end = datetime.now()  # Date now
         date_start_frame = date_end - date_start  # Starting interval data
@@ -76,18 +66,12 @@ def get_tick(
         data['mean_cost'] = data[['high', 'low']].mean(axis=1)
         data['sd_cost'] = data[['high', 'low']].std(axis=1)
 
-        plt.figure(figsize=(10, 5))
+        plt.figure(figsize=(15, 5))
 
         ax = sns.lineplot(data=data, x='begin', y='mean_cost', color='#0066FF', alpha=0.7, lw=2.5, label='Mean')
         ax.fill_between(data['begin'], data['mean_cost'] - data['sd_cost'], data['mean_cost'] + data['sd_cost'],
                         color='#99CCFF', alpha=0.5, label='Mean ± SD')
         sns.scatterplot(data=data, x='begin', y='close', s=45, color='#6699FF', alpha=0.7, label='Close')
-
-        if rolling_avg_num:
-            for val, color in zip(range(1, len(rolling_avg_int) + 1), colors):
-                data[f'SMA{val}'] = data['mean_cost'].rolling(window=val).mean()
-                sns.lineplot(data=data, x='begin', y=f'sma{val}', color='#33CC00', label=f'SMA {val} days', alpha=0.65,
-                             lw=2)
 
         ax.xaxis.set_major_formatter(date_format)
         ax.xaxis.set_major_locator(mdates.DayLocator(interval=1))
@@ -101,13 +85,12 @@ def get_tick(
                  fontstyle='oblique', fontvariant='small-caps')
         plt.legend(loc='lower left', ncols=2)
 
-        img = BytesIO()
-        plt.savefig(img, format='png', bbox_inches='tight')
-        plt.close()
-        img.seek(0)
+        # img = BytesIO()
+        # plt.savefig(img, format='png', bbox_inches='tight')
+        # plt.close()
+        # img.seek(0)
 
-        return img
-        # plt.show()
+        return plt.show()
 
     # Обработка ошибки запроса и возврат ошибки
     except requests.exceptions.RequestException as e:
