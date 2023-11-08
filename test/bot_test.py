e9de9d53120b325a6948e2fa5a075c01753626be
KK <kirill.krasnov64@gmail.com>
2023-11-08 23:20:32 +0300

    Добавил возможность ввода только тикета мосбиржи без интервалов

--- test/bot_test.py
+++ test/bot_test.py
@@ -1,13 +1,9 @@
-import base64
 import logging
-from concurrent.futures import ThreadPoolExecutor
-from io import BytesIO
 
-import matplotlib.pyplot as plt
 import telebot
 from telebot import types
 
-from __init__ import secrets  # Токен
+from __init__ import secrets
 from main_test import get_tick
 
 token = secrets.get('BOT_API_TOKEN')
@@ -15,10 +11,12 @@ bot = telebot.TeleBot(token)
 
 logging.basicConfig(level=logging.INFO)  # Настройка уровня логирования
 
-# Функция для отправки сообщения с клавиатурой
+
+# Функция для отправки сообщения с клавиатуры
 def send_keyboard(message, text, keyboard):
     logging.info(f'Отправка сообщения: {text}')
     bot.send_message(message.chat.id, text, reply_markup=keyboard)
+
 
 # Функция для обработки команды /start
 @bot.message_handler(commands=['start'])
@@ -31,19 +29,25 @@ def start_message(message):
     send_keyboard(message, f'Привет, {message.from_user.first_name}', keyboard)
     logging.info('Завершение обработки команды /start')
 
+
 # Функция для обработки кнопки 'ИНФО'
 @bot.message_handler(func=lambda message: message.text == 'Инфо')
 def handle_info(message):
     logging.info('Обработка кнопки Инфо')
-    send_keyboard(message, 'Я помогу тебе получить данные по стоимости акций с московской биржи и отобразить в виде графика', None)
+    send_keyboard(message,
+                  'Я помогу тебе получить данные по стоимости акций с московской биржи и отобразить в виде графика',
+                  None)
     logging.info('Завршение обработки кнопки Инфо')
+
 
 # Функция для обработки кнопки 'Акции'
 @bot.message_handler(func=lambda message: message.text == 'Акции')
 def handle_actions(message):
     logging.info('Handling Акции button')
-    send_keyboard(message, 'Какая акция интересует?\nПример команды:\nMOEX 4 недели\n\nВсе значения через пробелы', None)
+    send_keyboard(message, 'Какая акция интересует?\nПример команды:\nMOEX 4 недели\n\nВсе значения через пробелы',
+                  None)
     logging.info('Завршение обработки кнопки Акции')
+
 
 # Функция для обработки текста от пользователя
 @bot.message_handler(func=lambda message: True)
@@ -53,20 +57,37 @@ def handle_get_tick(message):
         logging.info('Обработка сообщения пользователя')
         chat_id = message.chat.id
         text_info = message.text.split()
-        tick, interval, interval_name = str(text_info[0]).upper(), int(text_info[1]), str(text_info[2]).lower()
 
-        logging.info(f'Запрос пользователя: тикер={tick}, интервал={interval}, дни/недели={interval_name}')
-        img = get_tick(tick=tick, interval_name=interval_name, interval=interval)
+        if len(text_info) > 1:
+            tick, interval, interval_name = str(text_info[0]).upper(), int(text_info[1]), str(text_info[2]).lower()
 
-        logging.info('Отправка сообщения пошльзователю')
-        img.seek(0)
-        bot.send_photo(chat_id, img)
-        img.close()
+            logging.info(f'Запрос пользователя: тикер={tick}, интервал={interval}, дни/недели={interval_name}')
+            img = get_tick(tick=tick, interval_name=interval_name, interval=interval)
+
+            logging.info('Отправка сообщения пошльзователю')
+            img.seek(0)
+            bot.send_photo(chat_id, img)
+            img.close()
+
+        elif len(text_info) == 1:
+            tick = text_info[0].upper()
+
+            logging.info(f'Запрос пользователя: тикер={tick}, интервал и дни/недели по-умолчанию')
+            img = get_tick(tick=tick)
+
+            logging.info('Отправка сообщения пошльзователю')
+            img.seek(0)
+            bot.send_photo(chat_id, img)
+            img.close()
+
+        else:
+            bot.send_message(chat_id, text='Неверный формат ввода. Убедитесь, что нет ошибки.')
 
         logging.info(f'Завершение запроса пользователя: тикер={tick}, интервал={interval}, дни/недели={interval_name}')
         logging.info('Запрос успешно обработан')
     except Exception as e:
         logging.error(f'Ошибка обработки запроса: {e}')
+
 
 if __name__ == "__main__":
     bot.polling(none_stop=True)
